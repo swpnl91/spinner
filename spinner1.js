3113876fa88056c6f855221c843218cfcfba412a
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-11-30 19:19:00 +0000

    adding spinner1.js

---
+++ spinner1.js
@@ -0,0 +1,75 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');  // Spaces are added for padding between the character and the cursor so that it doesn't block it out
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   '); 
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');  // Spaces are added for padding between the character and the cursor so that it doesn't block it out
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 2700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   '); 
+}, 2900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 3100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 3300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 3500);
